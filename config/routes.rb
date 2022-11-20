7d867efcec01156c5c50562e652df52feeab1bca
mluthfij <luthfujuliansyah48@gmail.com>
2022-11-20 22:08:03 +0700

    CRUD api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # api/v1/teacher
+
+  namespace :api do
+    namespace :v1 do
+      resources :teachers
+    end
+  end
 end
